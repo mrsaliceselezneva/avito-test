a88db5fdad3eb7d4e63a4f61702f07790ba3a29a
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2023-07-29 16:08:54 +0400

    начал делать главную страницу

--- src/pages/Main/View.jsx
+++ src/pages/Main/View.jsx
@@ -1,15 +1,16 @@
 import Menu from "components/Menu";
+
+import styles from "./styles.module.scss";
 import Search from "components/Search";
 import SearchButton from "components/SearchButton";
 import React from "react";
-import styles from "./styles.module.scss";
 
 const View = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.wrapper__main}>
                 <Search />
-                <div className={styles.wrapper__main__list} />
+                <div className={styles.wrapper__main__list}></div>
             </div>
             <div className={styles.wrapper__sidebar}>
                 <SearchButton />
