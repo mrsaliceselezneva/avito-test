9d73fcdd0645604d3e78ec8d19265d6b65c33cf0
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2023-08-19 20:58:30 +0400

    подключил авторизацию через гугл

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -9,12 +10,19 @@ import App from "./App";
 import { persistor, store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const google = `${process.env.REACT_APP_GOOGLE_CLIENT_ID}`;
+
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <PersistGate persistor={persistor}>
-                <App />
-            </PersistGate>
-        </BrowserRouter>
-    </Provider>,
+    <GoogleOAuthProvider clientId={google}>
+        <React.StrictMode>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <PersistGate persistor={persistor}>
+                        <App />
+                    </PersistGate>
+                </BrowserRouter>
+            </Provider>
+        </React.StrictMode>
+    </GoogleOAuthProvider>,
 );
